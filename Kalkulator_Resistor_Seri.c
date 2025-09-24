57662cd27363ca4f271a8410294dc5e6dc01281e
Moch. Damar Unggul Prayogo <damarunggul2006@gmail.com>
2025-09-24 23:30:38 +0700

    Remove and update resistor and number system calculators
    Deleted Kalkulator_ Resistor_Seri.c and Kalkulator_Desimal_OtherNumberSystem.c. Updated Kalkulator_Resistor_Seri.c with new structure and input prompt for resistor series calculator.

--- Kalkulator_Resistor_Seri.c
+++ Kalkulator_Resistor_Seri.c
@@ -1,1 +1,13 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+
+int main (){
+
+    int nomor;
+    printf("---Kalkulator Resistor Seri---");
+    printf("Masukkan berapa resistor yang ada dalam rangkaian seri: ");
+    scanf()
+
+}
