beb9ed7db8da87b69250a1f61366bbf6f6328145
Moch. Damar Unggul Prayogo <damarunggul2006@gmail.com>
2025-11-19 03:00:19 +0700

    Add decimal conversion calculator and rename file
    Added Kalkulator_Konversi_Desimal.c for converting decimal to binary and octal. Renamed 'project kalkulator dan konversi.c' to 'project_kalkulator_dan_konversi.c' for consistency.

---
+++ Kalkulator_Konversi_Desimal.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
+
+int desimalKeBiner(int desimal, int biner[32]){
+    int i = 0;
+    while(desimal > 0){
+        biner[i] = desimal % 2;
+        desimal /= 2;
+        i++;
+    }
+    printf("Bilangan Biner: ");
+    for(int j = i - 1; j >= 0; j--){
+        printf("%d", biner[j]);
+    }
+}
+
+int desimalKeOktal(int desimal, int Oktal[32]){
+    int i = 0;
+    while(desimal > 0){
+        Oktal[i] = desimal % 8;
+        desimal /= 8;
+        i++;
+    }
+    printf("\nBilangan Oktal: ");
+    for (int j = i - 1; j >= 0; j--){
+        printf("%d", Oktal[j]);
+    }
+
+}
+
+int main(){
+    int desimal;
+    int biner[32], Oktal[32], Heksadesimal[32];
+    printf("===SELAMAT DATANG DI KALKULATOR KONVERSI DeBOH (Desimal ke Biner, Oktal, dan Heksadesimal)===\n\n");
+    printf("Masukkan nilai desimal: ");
+    scanf("%d", &desimal);
+    printf("\nHasil Konversi: \n");
+    desimalKeBiner(desimal, biner);
+    desimalKeOktal(desimal, Oktal);
+
+
+
+}
