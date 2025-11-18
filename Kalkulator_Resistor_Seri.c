6c7946274f377bf622e6a245716fef71989c610e
Moch. Damar Unggul Prayogo <damarunggul2006@gmail.com>
2025-11-19 01:59:58 +0700

    Implement resistor series calculator logic
    Added logic to calculate the equivalent resistance for a series of resistors based on user input. The program now prompts for the number of resistors and their values, then outputs the total equivalent resistance.

--- Kalkulator_Resistor_Seri.c
+++ Kalkulator_Resistor_Seri.c
@@ -1,13 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-#include <string.h>
 
 int main (){
 
     int nomor;
-    printf("---Kalkulator Resistor Seri---");
-    printf("Masukkan berapa resistor yang ada dalam rangkaian seri: ");
-    scanf()
+    int resistor_ekivalen = 0;
+    printf("---Kalkulator Resistor Seri---\n");
+    printf("\nMasukkan berapa resistor yang ada dalam rangkaian seri: ");
+    scanf("%d", &nomor);
+
+    for (int i = 1; i <= nomor; i++){
+        int resistor;
+        printf("Resitor ke-%d: ", i);
+        scanf("%d", &resistor);
+        resistor_ekivalen += resistor;
+
+    }
+    printf("\nJadi, nilai dari resistor ekivalennya adalah: %d\n", resistor_ekivalen);
+    printf("\nTekan ENTER untuk lanjut.");
+    getchar(); getchar();
+
+    return 0;
 
 }
